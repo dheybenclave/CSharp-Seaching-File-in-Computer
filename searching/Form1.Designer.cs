3b374515a714e399ecac12cc9c57077d841668b6
dheybenclave <dheybenclave@gmail.com>
2018-03-25 14:58:24 -0700

    Upload March 25, 2018
    Searching all File in Computer

---
+++ searching/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace searching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDrivers = new System.Windows.Forms.ComboBox();
+            this.lbxFiles = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbDrivers
+            // 
+            this.cmbDrivers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDrivers.FormattingEnabled = true;
+            this.cmbDrivers.Location = new System.Drawing.Point(12, 26);
+            this.cmbDrivers.Name = "cmbDrivers";
+            this.cmbDrivers.Size = new System.Drawing.Size(48, 24);
+            this.cmbDrivers.TabIndex = 0;
+            this.cmbDrivers.SelectedIndexChanged += new System.EventHandler(this.cmbDrivers_SelectedIndexChanged);
+            // 
+            // lbxFiles
+            // 
+            this.lbxFiles.FormattingEnabled = true;
+            this.lbxFiles.ItemHeight = 16;
+            this.lbxFiles.Location = new System.Drawing.Point(12, 56);
+            this.lbxFiles.Name = "lbxFiles";
+            this.lbxFiles.Size = new System.Drawing.Size(1020, 644);
+            this.lbxFiles.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 718);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbxFiles);
+            this.Controls.Add(this.cmbDrivers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDrivers;
+        private System.Windows.Forms.ListBox lbxFiles;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ searching/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace searching
+{
+    public partial class Form1 : Form
+    {
+        List<string> directories = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDrives();
+        }
+        private void LoadDrives()
+        {
+            string[] drv = Directory.GetLogicalDrives();
+            for (int i = 0; i < drv.Length; i++)
+            {
+                try
+                {
+                    Directory.GetDirectories(drv[i]);
+                    cmbDrivers.Items.Add(drv[i].ToString());
+                }
+                catch { }
+                
+            }
+            cmbDrivers.SelectedItem = cmbDrivers.Items[1];
+            SearchDirectory(@cmbDrivers.SelectedItem.ToString());
+        }
+        public void SearchDirectory(string drive)
+        {//@cmbDrivers.SelectedItem.ToString()
+            string[] dirs = Directory.GetDirectories(drive);
+            directories.AddRange(dirs);
+            int count = 0;
+            int max = 0;
+            int lastCount = 0;
+            
+            
+            while (true)
+            {
+                List<string> listDirs = new List<string>();
+                max = directories.Count;
+                for (int i = lastCount; i < max; i++)
+                {
+                    string[] subDirs = new string[0];
+                    try
+                    {
+                        subDirs = Directory.GetDirectories(directories[i]);
+                        if (subDirs.Length != 0)
+                        {
+                            listDirs.AddRange(subDirs);
+                        }
+                        else
+                            count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        count++;
+                    }
+                }
+                if (count == (max - lastCount))
+                    break;
+                else
+                {
+                    count = 0;
+                    directories.AddRange(listDirs);
+                }
+                lastCount = max;
+
+               
+            }
+            SetToBox();
+        }
+        public void SetToBox()
+        {
+            for (int i = 0; i < directories.Count; i++)
+            {
+                string[] dir = new string[0];
+                try
+                {
+                    dir = Directory.GetFiles(directories[i], "*.txt");
+                    if (dir.Length != 0)
+                    {
+                        foreach (string s in dir)
+                        {
+                            lbxFiles.Items.Add(s);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        private void cmbDrivers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbxFiles.Items.Clear();
+            directories.Clear();
+            SearchDirectory(cmbDrivers.SelectedItem.ToString());
+        }
+    }
+}
